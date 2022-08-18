5c47a729b285924a0bb8a5fa4767f86d69437dd9
Bidisha Das <officialbidisha1@gmail.com>
2022-08-18 13:38:42 +0530

    Added port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server Started at ${3000}`)
 })
 
