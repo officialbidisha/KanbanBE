a6075a4993c91a236615d74935737cfdd12b9bc2
Bidisha Das <officialbidisha1@gmail.com>
2022-08-18 13:44:04 +0530

    Adding cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.listen(port, () => {
     console.log(`Server Started at ${3000}`)
 })
