d305dbb86c10cc6a7da723dbf2ad64fce168eb46
Bidisha Das <officialbidisha1@gmail.com>
2023-09-29 17:45:21 +0530

    Cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: true })); 
 app.listen(port, () => {
     console.log(`Server Started at ${3000}`)
 })
